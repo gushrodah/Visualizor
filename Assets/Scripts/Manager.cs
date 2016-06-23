1059e8ec129a2a84a65e4b5c9f69b010556109f2
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-06-22 18:06:32 -0700

    bars work

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -10,46 +10,70 @@ public class Manager : MonoBehaviour {
     // Light Objects
     public GameObject L1, L2, L3;
     Light bassL, snareL, melody;
+    public GameObject bassCube, snareCube;
 
     // Cube Objects
-    GameObject[] cubes;
+    public GameObject[] bassCubes;
+    public GameObject[] snareCubes;
+    GameObject bass, snare;
 
     void Start () {
         bassL = L1.GetComponent<Light>();
         snareL = L2.GetComponent<Light>();
         melody = L3.GetComponent<Light>();
 
-        CreateCubes(10);
+        CreateCubes(10);    // create cube data
     }
 	
 	void Update () {
         ChangeRange(SpecData.freqData[50]);
-        snareL.range = SpecData.freqData[240] * 800;
-        melody.range = SpecData.freqData[150] * 300;
+        //snareL.range = SpecData.freqData[240] * 800;
+        //melody.range = SpecData.freqData[150] * 300;
 
-        SpecData.ReduceData(64);
+        //SpecData.ReduceData(64);
         UpdateCubes();
         //Testing();
     }
 
     void CreateCubes(int radius)
     {
+        bass = new GameObject();
+        snare = new GameObject();
+        int size = SpecData.bassSamples.Length;
         //SpecData.ReduceData(64);
-        cubes = new GameObject[SpecData.reducedData.Length];
-        for (int i = 0; i < SpecData.reducedData.Length; i++)
+        bassCubes = new GameObject[size];
+        snareCubes = new GameObject[size];
+        for (int i = 0; i < size; i++)
         {
-            float angle = i * Mathf.PI * 2 / SpecData.reducedData.Length;
+            float angle = i * Mathf.PI * 2 / size;
             Vector3 pos = new Vector3(Mathf.Cos(angle) * radius, -3, Mathf.Sin(angle) * radius + 2);
-            cubes[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            bassCubes[i] = Instantiate(bassCube) as GameObject;
+            bassCubes[i].transform.position = pos;
+            bassCubes[i].transform.SetParent(bass.transform);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            float angle = i * Mathf.PI * 2 / size;
+            Vector3 pos = new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius + 2);
+            snareCubes[i] = Instantiate(snareCube) as GameObject;
+            snareCubes[i].transform.position = pos;
+            snareCubes[i].transform.SetParent(snare.transform);
         }
     }
 
     void UpdateCubes()
     {
-        for(int i = 0; i < cubes.Length; i++)
+        for (int i = 0; i < bassCubes.Length; i++)
         {
-            cubes[i].transform.localScale = new Vector3(1, SpecData.reducedData[i]);
+            bassCubes[i].transform.localScale = new Vector3(1, SpecData.bassSamples[i] * 128, 1);
+            //bassCubes[i].transform.Translate(new Vector3(0, SpecData.bassSamples[i] * 2), 0);
         }
+        bass.transform.Rotate(new Vector3(10, 10, 0) * Time.deltaTime);
+        for (int i = 0; i < snareCubes.Length; i++)
+        {
+            snareCubes[i].transform.localScale = new Vector3(1, SpecData.snareSamples[i] * 400, 1);
+        }
+        snare.transform.Rotate(new Vector3(0, 10, 10) * Time.deltaTime);
     }
 
     void ChangeScale()
@@ -71,7 +95,7 @@ public class Manager : MonoBehaviour {
         //    Debug.DrawLine(new Vector3(i - 1, Mathf.Log(SpecData.freqData[i]) + 10, 2), new Vector3(i, Mathf.Log(SpecData.freqData[i]) + 10, 2), Color.cyan);
         //}
         // test reduced samples
-        SpecData.ReduceData(64);
+        //SpecData.ReduceData(64);
         for (int i = 0; i < SpecData.reducedData.Length - 1; i++)
         {
             Debug.DrawLine(new Vector3(i - 1, SpecData.reducedData[i] + 10, 0), new Vector3(i, SpecData.reducedData[i + 1] + 10, 0), Color.magenta);
--- Assets/Scripts/specAnalyzer.cs
+++ Assets/Scripts/specAnalyzer.cs
@@ -7,6 +7,9 @@ public class specAnalyzer : MonoBehaviour {
     AudioSource audioSource;
     public float []freqData;                // all samples
     public int numSamples;
+    public float []bassSamples;
+    public float []midSamples;
+    public float []snareSamples;
     public float[] reducedData;
 
     public float numFrames;
@@ -14,6 +17,8 @@ public class specAnalyzer : MonoBehaviour {
     void Start () {
         audioSource = GetComponent<AudioSource>();
         freqData    = new float[numSamples];
+        bassSamples = new float[50];
+        snareSamples = new float[50];
                
         // get data 15 fps
         InvokeRepeating("ExtractData", 0, 1.0f / numFrames);
@@ -22,6 +27,31 @@ public class specAnalyzer : MonoBehaviour {
     void ExtractData()
     {
         audioSource.GetSpectrumData(freqData, 0, FFTWindow.Rectangular);
+        // bass samples = 200
+        int index = 0;
+        for(int i = 0; i < 200; i+=4)
+        {
+            bassSamples[index] = freqData[i];
+            for(int j = 1; j < 4; j++)
+            {
+                float l = bassSamples[index];
+                float r = freqData[i + j];
+                bassSamples[index] = (l > r) ? l : r;
+            }
+            index++;
+        }
+        index = 0;
+        for (int i = 1000; i < 4000; i += 60)
+        {
+            snareSamples[index] = freqData[i];
+            for (int j = 1; j < 60; j++)
+            {
+                float l = snareSamples[index];
+                float r = freqData[i + j];
+                snareSamples[index] = (l > r) ? l : r;
+            }
+            index++;
+        }
     }
 
     // newSampleSize has to be mult of 2
